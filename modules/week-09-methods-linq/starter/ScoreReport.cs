6fba22833b2e4ff1030202254def492270f2142c
Aatiqa <aatiqa598@gmail.com>
2026-03-11 17:46:56 -0700

    Update ScoreReport.cs

--- modules/week-09-methods-linq/starter/ScoreReport.cs
+++ modules/week-09-methods-linq/starter/ScoreReport.cs
@@ -64,7 +64,7 @@ internal class ScoreReport
     {
         int min = _scores.Min();
         int max = _scores.Max();
-        double average = _ores.Average();
+        double average = _scores.Average();
         int count = _scores.Count();
         Console.WriteLine($"Count: {count}");
         Console.WriteLine($"Min: {min}");
